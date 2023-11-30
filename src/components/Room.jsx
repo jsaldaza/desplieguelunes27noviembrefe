41a5fe77ef131f99dc28eebf35efe7682b567741
jsaldaza <jsaldaza@gmail.com>
2023-11-30 18:05:22 -0500

    Se sube el proyecto de front para su integracion con cambios

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -12,7 +12,7 @@ const Room = ({
   return (
     <div className="room">
       <picture className="room_pic">
-        {/* <button className="room_reservar">Reservar</button> */}
+        <button className="room_reservar"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg></button>
         <img src={imagen} loading="lazy"/>
         <svg className="room_loading" xmlns="http://www.w3.org/2000/svg" width="76" height="76" fill="#777777" viewBox="0 0 256 256"><path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path></svg>
       </picture >
